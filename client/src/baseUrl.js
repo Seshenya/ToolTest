61c334da670eb06fafdd09e0813a20aeaeda6eca
DipeshK1999 <dipesh.kewalramani@informatik@hs-fulda.de>
2023-12-03 20:19:07 +0100

    Update Base url

--- client/src/baseUrl.js
+++ client/src/baseUrl.js
@@ -1,1 +1,2 @@
-export const baseUrl = "http://localhost:4000"
+// export const baseUrl = "http://localhost:4000"
+export const baseUrl = "http://74.234.41.238"
--- client/src/layouts/shop/index.js
+++ client/src/layouts/shop/index.js
@@ -86,7 +86,7 @@ function Shop() {
 
   useEffect(() => {
     getMedia()
-  })
+  }, [])
 
   return (
     <DashboardLayout>
