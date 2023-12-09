2a739dcebc8f9721278fc7b918c1f72d35c3032b
Seshenya-Weerasinghe <seshenya.weerasinghe@informatik.hs-fulda.de>
2023-12-09 19:28:54 +0100

    added refresh token

--- server/src/middleware/has-token.ts
+++ server/src/middleware/has-token.ts
@@ -5,12 +5,12 @@ function hasToken(req: any, res: any, next: any) {
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
     if (!token) {
-        return res.status(401).send({ reason: 'Missing access token' })
+        return res.status(401).send({ message: 'Missing access token' })
     }
 
     jwt.verify(token, config.ACCESS_TOKEN_SECRET, (err: any) => {
         if (err) {
-            return res.status(401).send({ reason: 'Token is invalid' })
+            return res.status(401).send({ message: 'Token is invalid' })
         }
         next()
     })
