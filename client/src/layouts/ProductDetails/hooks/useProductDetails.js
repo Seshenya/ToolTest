6eb37d5a0586e52d77d346fdb82d381dd08669a4
Alex Francis <francisalex619@gmail.com>
2023-12-11 01:12:59 +0530

    Added private axios

--- client/src/layouts/ProductDetails/hooks/useProductDetails.js
+++ client/src/layouts/ProductDetails/hooks/useProductDetails.js
@@ -1,41 +1,45 @@
-const { useState, useEffect, useCallback } = require('react');
-const { getProductDetails } = require('../services/productDetailsServices.service');
+import useAxiosPrivate from 'hooks/useAxiosPrivate'
+import { useCallback, useEffect, useState } from 'react'
 
 const initSb = {
-  open: false,
-  color: '',
-  icon: '',
-  title: '',
-  message: '',
-};
+    open: false,
+    color: '',
+    icon: '',
+    title: '',
+    message: '',
+}
 
-const useProductDetails = productId => {
-  const [productDetails, setProductDetails] = useState(null);
-  const [sb, setSb] = useState({ ...initSb });
-  const fetchProductDetails = useCallback(async () => {
-    try {
-      const resp = await getProductDetails(productId);
-      setProductDetails(resp);
-    } catch (error) {
-      setSb({
-        open: true,
-        color: 'error',
-        icon: 'error',
-        title: error.message,
-        message: '',
-      });
+const useProductDetails = (productId) => {
+    const [productDetails, setProductDetails] = useState(null)
+    const [sb, setSb] = useState({ ...initSb })
+    const axiosPrivate = useAxiosPrivate()
+
+    const fetchProductDetails = useCallback(async () => {
+        axiosPrivate
+            .get(`/media/${productId}`)
+            .then((resp) => {
+                setProductDetails(resp.data)
+            })
+            .catch((error) => {
+                setSb({
+                    open: true,
+                    color: 'error',
+                    icon: 'error',
+                    title: error.message,
+                    message: '',
+                })
+            })
+    }, [axiosPrivate, productId])
+
+    useEffect(() => {
+        fetchProductDetails()
+    }, [fetchProductDetails])
+
+    const closeSb = () => {
+        setSb({ ...initSb })
     }
-  }, [productId]);
 
-  useEffect(() => {
-    fetchProductDetails();
-  }, [fetchProductDetails]);
+    return { productDetails, sb, closeSb }
+}
 
-  const closeSb = () => {
-    setSb({ ...initSb });
-  };
-
-  return { productDetails, sb, closeSb };
-};
-
-export default useProductDetails;
+export default useProductDetails
--- client/src/layouts/ProductDetails/services/productDetailsServices.service.js
+++ client/src/layouts/ProductDetails/services/productDetailsServices.service.js
@@ -1,16 +1,16 @@
-import axios from 'axios';
-import { baseUrl } from 'baseUrl';
+import { axiosPrivate } from 'api/axios'
+import { baseUrl } from 'baseUrl'
 
-const getProductDetails = productId => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const resp = await axios.get(`${baseUrl}/media/${productId}`);
-      console.log('service');
-      resolve(resp.data);
-    } catch (error) {
-      reject(error);
-    }
-  });
-};
+const getProductDetails = (productId) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const resp = await axiosPrivate.get(`${baseUrl}/media/${productId}`)
+            console.log('service')
+            resolve(resp.data)
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
 
-export { getProductDetails };
+export { getProductDetails }
