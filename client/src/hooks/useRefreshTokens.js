f658d31dc2f0fb6ccb16833e2df23ed6dbc8d515
DipeshK1999 <dipesh.kewalramani@informatik@hs-fulda.de>
2023-12-16 01:16:21 +0100

    logout user if refresh token expires

--- client/src/hooks/useRefreshTokens.js
+++ client/src/hooks/useRefreshTokens.js
@@ -1,15 +1,25 @@
+import { useNavigate } from 'react-router-dom';
 import axios from '../api/axios';
 import useAuth from './useAuth';
 
 const useRefreshToken = () => {
     const { updateAuth, auth } = useAuth();
+    const navigate = useNavigate();
 
     const refresh = async () => {
-        const response = await axios.post('/token', {
-            token: auth?.refreshToken
-        });
-        updateAuth({ ...auth, accessToken: response?.data?.accessToken });
-        return response?.data?.accessToken;
+        try {
+            const response = await axios.post('/token', {
+                token: auth?.refreshToken
+            });
+            updateAuth({ ...auth, accessToken: response?.data?.accessToken });
+            return response?.data?.accessToken;
+        } catch (error) {
+            console.log(error)
+            updateAuth({})
+            localStorage.removeItem("user")
+            navigate('/authentication/sign-in')
+        }
+
     }
     return refresh;
 };
