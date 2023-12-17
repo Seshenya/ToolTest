f4469f8b00382403ec77e87809189f97bdbc58c3
GihanKaushal <gihaankaushal@gmail.com>
2023-12-17 14:03:08 +0100

    update user profile fe

--- client/src/layouts/profile/index.js
+++ client/src/layouts/profile/index.js
@@ -1,15 +1,27 @@
+import React, { useState, useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import useAxiosPrivate from 'hooks/useAxiosPrivate';
+import useAuth from "hooks/useAuth";
+import useUserDetails from './hooks/useUserDetails';
 
 // @mui material components
+import Card from "@mui/material/Card";
 import Grid from "@mui/material/Grid";
 import Divider from "@mui/material/Divider";
+import Icon from "@mui/material/Icon";
 
-// @mui icons
-import FacebookIcon from "@mui/icons-material/Facebook";
-import TwitterIcon from "@mui/icons-material/Twitter";
-import InstagramIcon from "@mui/icons-material/Instagram";
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogActions from '@mui/material/DialogActions';
+import TextField from '@mui/material/TextField';
 
 // Material Dashboard 2 React components
 import MDBox from "components/MDBox";
+import MDTypography from "components/MDTypography";
+import MDAvatar from "components/MDAvatar";
+import MDButton from "components/MDButton";
+import MDSnackbar from 'components/MDSnackbar';
 
 // Material Dashboard 2 React example components
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
@@ -17,160 +29,215 @@ import DashboardNavbar from "examples/Navbars/DashboardNavbar";
 import Footer from "examples/Footer";
 import ProfileInfoCard from "examples/Cards/InfoCards/ProfileInfoCard";
 
-// Overview page components
-import Header from "layouts/profile/components/Header";
-
-// Data
-
 // Images
+import backgroundImage from "assets/images/bg-profile.jpeg";
 
-function Overview() {
+const UserDetails = () => {
+  const { auth } = useAuth();
+  const { userDetails, sb, closeSb } = useUserDetails(auth.user_id); 
+  const [userData, setUserData] = useState(null);
+  const [openModal, setOpenModal] = useState(false);
+  const { register, handleSubmit, reset, setValue } = useForm();
+  const axiosPrivate = useAxiosPrivate()
+
+  useEffect(() => {
+    setUserData(userDetails);
+  }, [userDetails]);
+
+  console.log("userDetails", userDetails)
+  console.log("userData", userData)
+
+  const handleModalOpen = () => {
+    setOpenModal(true);
+  };
+
+  const handleModalClose = () => {
+    setOpenModal(false);
+  };
+
+  //user profile update
+  const handleUserUpdate = async (formData) => {
+    try {
+      const response = await axiosPrivate.put(`/users/${auth.user_id}`, formData, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (response.status === 200) {
+        const data = response.data;
+        console.log('User updated:', data);
+        setUserData(data);
+
+        setOpenModal(false);
+        reset();
+      } else {
+        console.error('Failed to update user');
+      }
+    } catch (error) {
+      console.error('Error updating user:', error);
+    }
+  };
+
+  const onSubmit = (data) => {
+    console.log("On Submit:", data);
+
+    const formData = {
+      firstname: data.firstname || undefined,
+      lastname: data.lastname || undefined,
+      description: data.description || undefined,
+      skills: data.skills || undefined,
+    };
+
+    console.log("Form Data:", formData);
+
+    handleUserUpdate(formData);
+    
+    setOpenModal(false);
+    reset();
+  };
+
+  const handleFormReset = () => {
+    reset();
+  };
+
   return (
     <DashboardLayout>
       <DashboardNavbar />
       <MDBox mb={2} />
-      <Header>
-        <MDBox mt={5} mb={3}>
-          <Grid container spacing={1}>
-            {/* <Grid item xs={12} md={6} xl={4}>
-              <PlatformSettings />
-            </Grid> */}
-            <Grid item xs={12} md={12} xl={12} sx={{ display: "flex" }}>
-              <Divider orientation="vertical" sx={{ ml: -2, mr: 1 }} />
-              <ProfileInfoCard
-                title="profile"
-                description="Hi, I’m John Doe, Decisions: If you can’t decide, the answer is no. If two equally difficult paths, choose the one more painful in the short term (pain avoidance is creating an illusion of equality)."
-                info={{
-                  fullName: "John Doe",
-                  email: "johndoe@hs-fulda.com",
-                  username: "john23",
-                }}
-                social={[
-                  {
-                    link: "https://www.facebook.com/CreativeTim/",
-                    icon: <FacebookIcon />,
-                    color: "facebook",
-                  },
-                  {
-                    link: "https://twitter.com/creativetim",
-                    icon: <TwitterIcon />,
-                    color: "twitter",
-                  },
-                  {
-                    link: "https://www.instagram.com/creativetimofficial/",
-                    icon: <InstagramIcon />,
-                    color: "instagram",
-                  },
-                ]}
-                skills={['Figma', 'Blender', '3D Animation']}
-                action={{ route: "", tooltip: "Edit Profile" }}
-                shadow={false}
-              />
-              <Divider orientation="vertical" sx={{ mx: 0 }} />
+        <MDBox position="relative" mb={5}>
+          <MDBox
+            display="flex"
+            alignItems="center"
+            position="relative"
+            minHeight="18.75rem"
+            borderRadius="xl"
+            sx={{
+              backgroundImage: ({ functions: { rgba, linearGradient }, palette: { gradients } }) =>
+                `${linearGradient(
+                  rgba(gradients.info.main, 0.6),
+                  rgba(gradients.info.state, 0.6)
+                )}, url(${backgroundImage})`,
+              backgroundSize: "cover",
+              backgroundPosition: "50%",
+              overflow: "hidden",
+            }}
+          />
+          <Card
+            sx={{
+              position: "relative",
+              mt: -8,
+              mx: 3,
+              py: 2,
+              px: 2,
+            }}
+          >
+            <Grid container spacing={3} alignItems="center">
+              <Grid item>
+                <MDAvatar src={userDetails?.profile_picture} alt="profile-image" size="xl" shadow="sm" />
+              </Grid>
+              <Grid item>
+                <MDBox height="100%" mt={0.5} lineHeight={1}>
+                  <MDTypography variant="h5" fontWeight="medium">
+                    {userDetails?.firstname} {userDetails?.lastname}
+                  </MDTypography>
+                </MDBox>
+              </Grid>
             </Grid>
-            {/* <Grid item xs={12} xl={4}>
-              <ChatList title="conversations" profiles={ChatListData} shadow={false} />
-            </Grid> */}
-          </Grid>
+            <MDBox mt={5} mb={3}>
+              <Grid container spacing={1}>
+                <MDButton
+                  variant="gradient"
+                  size="small"
+                  color={'primary'}
+                  onClick={handleModalOpen}
+                >
+                  <Icon>edit</Icon>
+                  Edit
+                </MDButton>
+                <Grid item xs={12} md={12} xl={12} sx={{ display: "flex" }}>
+                
+                  <Divider orientation="vertical" sx={{ ml: -2, mr: 1 }} />
+                  <ProfileInfoCard
+                    title="profile"
+                    description= {userData?.description}
+                    info={{
+                      fullName: `${userData?.firstname} ${userData?.lastname}`,
+                      email: userData?.email,
+                      username: userData?.username,
+                      skills: userData?.skills
+                    }}
+                    shadow={false}
+                  />
+                  <Divider orientation="vertical" sx={{ mx: 0 }} />
+                </Grid>
+              </Grid>
+            </MDBox>
+          </Card>
         </MDBox>
-        {/* <MDBox pt={2} px={2} lineHeight={1.25}>
-          <MDTypography variant="h6" fontWeight="medium">
-            Projects
-          </MDTypography>
-          <MDBox mb={1}>
-            <MDTypography variant="button" color="text">
-              Architects design houses
-            </MDTypography>
-          </MDBox>
-        </MDBox> */}
-        {/* <MDBox p={2}>
-          <Grid container spacing={6}>
-            <Grid item xs={12} md={6} xl={3}>
-              <DefaultProjectCard
-                image={homeDecor1}
-                label="project #2"
-                title="modern"
-                description="As Uber works through a huge amount of internal management turmoil."
-                action={{
-                  type: "internal",
-                  route: "/pages/profile/profile-overview",
-                  color: "info",
-                  label: "view project",
-                }}
-                authors={[
-                  { image: team1, name: "Elena Morison" },
-                  { image: team2, name: "Ryan Milly" },
-                  { image: team3, name: "Nick Daniel" },
-                  { image: team4, name: "Peterson" },
-                ]}
-              />
-            </Grid>
-            <Grid item xs={12} md={6} xl={3}>
-              <DefaultProjectCard
-                image={homeDecor2}
-                label="project #1"
-                title="scandinavian"
-                description="Music is something that everyone has their own specific opinion about."
-                action={{
-                  type: "internal",
-                  route: "/pages/profile/profile-overview",
-                  color: "info",
-                  label: "view project",
-                }}
-                authors={[
-                  { image: team3, name: "Nick Daniel" },
-                  { image: team4, name: "Peterson" },
-                  { image: team1, name: "Elena Morison" },
-                  { image: team2, name: "Ryan Milly" },
-                ]}
-              />
-            </Grid>
-            <Grid item xs={12} md={6} xl={3}>
-              <DefaultProjectCard
-                image={homeDecor3}
-                label="project #3"
-                title="minimalist"
-                description="Different people have different taste, and various types of music."
-                action={{
-                  type: "internal",
-                  route: "/pages/profile/profile-overview",
-                  color: "info",
-                  label: "view project",
-                }}
-                authors={[
-                  { image: team4, name: "Peterson" },
-                  { image: team3, name: "Nick Daniel" },
-                  { image: team2, name: "Ryan Milly" },
-                  { image: team1, name: "Elena Morison" },
-                ]}
-              />
-            </Grid>
-            <Grid item xs={12} md={6} xl={3}>
-              <DefaultProjectCard
-                image={homeDecor4}
-                label="project #4"
-                title="gothic"
-                description="Why would anyone pick blue over pink? Pink is obviously a better color."
-                action={{
-                  type: "internal",
-                  route: "/pages/profile/profile-overview",
-                  color: "info",
-                  label: "view project",
-                }}
-                authors={[
-                  { image: team4, name: "Peterson" },
-                  { image: team3, name: "Nick Daniel" },
-                  { image: team2, name: "Ryan Milly" },
-                  { image: team1, name: "Elena Morison" },
-                ]}
-              />
-            </Grid>
-          </Grid>
-        </MDBox> */}
-      </Header>
       <Footer />
+      <Dialog open={openModal} onClose={handleModalClose} fullScreen>
+        <DialogTitle id="update-status-title">Update User</DialogTitle>
+        <DialogContent>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <TextField
+              {...register('firstname')}
+              label="First Name"
+              fullWidth
+              margin="normal"
+              variant="outlined"
+              sx={{ marginBottom: 2 }}
+            />
+            <TextField
+              {...register('lastname')}
+              label="Last Name"
+              fullWidth
+              margin="normal"
+              variant="outlined"
+              sx={{ marginBottom: 2 }}
+            />
+            <TextField
+              {...register('description')}
+              label="Description"
+              multiline
+              fullWidth
+              rows={6}
+              margin="normal"
+              variant="outlined"
+              sx={{ marginBottom: 2 }}
+            />
+            <TextField
+              {...register('skills')}
+              label="Skills"
+              multiline
+              fullWidth
+              rows={6}
+              margin="normal"
+              variant="outlined"
+              sx={{ marginBottom: 2 }}
+            />
+            <DialogActions>
+              <MDButton type="submit" variant="contained" color="primary" sx={{ marginRight: 2 }}>
+                Save
+              </MDButton>
+              <MDButton onClick={handleFormReset} color="secondary">Reset</MDButton>
+              <MDButton onClick={handleModalClose}>Cancel</MDButton>
+            </DialogActions>
+          </form>
+        </DialogContent>
+      </Dialog>
+      <MDSnackbar
+        color={sb.color}
+        icon={sb.icon}
+        title={sb.title}
+        content={sb.message}
+        open={sb.open}
+        onClose={closeSb}
+        close={closeSb}
+        bgWhite
+      />
     </DashboardLayout>
   );
 }
 
-export default Overview;
+export default UserDetails;
