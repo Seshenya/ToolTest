cded1e3e0dcd09de8a0f3c2771df7c118516f29c
GihanKaushal <gihaankaushal@gmail.com>
2023-12-25 02:05:23 +0100

    update user profile

--- server/src/user/api/user.ts
+++ server/src/user/api/user.ts
@@ -1,4 +1,5 @@
 import { getUser, getUsers, createUser, alterUser } from '../services'
+import formidable from 'express-formidable';
 
 async function fetchUser(req: any, res: any) {
     getUser(req.params.id)
@@ -31,13 +32,24 @@ async function addUser(req: any, res: any) {
 }
 
 async function updateUser(req: any, res: any) {
-    alterUser(req.params.id, req.body)
-        .then((user) => {
-            res.send(user)
-        })
-        .catch((error) => {
-            res.status(400).send({ message: error })
-        })
+    formidable({ multiples: true })(req, res, async (err) => {
+        if (err) {
+            return res.status(500).json({ error: 'File upload failed.' })
+        }
+
+        const userData = {
+            fields: req.fields,
+            fileProfilePicture: req.files.profile_picture,
+        }
+
+        await alterUser(req.params.id, userData)
+            .then((user) => {
+                res.send(user)
+            })
+            .catch((error) => {
+                res.status(500).send({ message: 'Error updating user' })
+            })
+    })
 }
 
 export { fetchUser, fetchUsers, addUser, updateUser }
--- server/src/user/services/user.ts
+++ server/src/user/services/user.ts
@@ -1,18 +1,40 @@
 import { hashPassword } from './index'
 import { User } from '../entities'
 import { UserType } from '../types'
+import { promises as fsPromises } from 'fs'
+import { generateSASUrl } from '../../middleware/fetch-media-blob-storage'
+import { storeBlobToBlobStorage } from '../../middleware/store-media-blob-storage'
+import { error } from 'console'
+
+interface UserData {
+    fields: any
+    fileProfilePicture: any
+}
 
 async function getUser(user_id: number) {
+    const user = await User.findOneBy({
+        user_id,
+    })
+
+    if (!user) {
+        throw new Error('User not found')
+    }
+
+    const containerName = 'gdsdt4'
+    const blobNameUserProfile = user.profile_picture
+
     try {
-        const user = await User.findOneBy({
-            user_id,
-        })
-        return user
-    } catch (err: any) {
-        // eslint-disable-next-line no-console
-        console.log('Error :', err)
-        throw err
-    } 
+        const blobUrlWithSAS = await generateSASUrl(
+            containerName,
+            blobNameUserProfile
+        )
+        user.profile_picture = blobUrlWithSAS
+    } catch (error) {
+        throw new Error(`Error generating SAS URL for ${blobNameUserProfile}`)
+    }
+    
+    user.password = ""
+    return user
 }  
 
 async function getUsers() {
@@ -61,8 +83,8 @@ async function createUser(user: UserType) {
     }
 }
 
-async function alterUser(user_id: number, user: UserType) {
-    const {firstname, lastname, description, skills} = user
+async function alterUser(user_id: number, user: UserData) {
+    const {firstname, lastname, description, skills} = user.fields
 
     try {
         const existingUser = await User.findOneBy({ user_id })
@@ -89,6 +111,18 @@ async function alterUser(user_id: number, user: UserType) {
             updateObject.skills = skills
         }
 
+        if (user.fileProfilePicture !== undefined) {
+            const containerName = 'gdsdt4'
+
+            // Add Media to Azure Blob Storage
+            const blobNameProfilePic = `user_${firstname}_${lastname}_${Date.now()}_${Math.random()}_${user.fileProfilePicture.name}`
+            const dataMedia = await fsPromises.readFile(user.fileProfilePicture.path)
+
+            storeBlobToBlobStorage(containerName, blobNameProfilePic, dataMedia)
+
+            updateObject.profile_picture = blobNameProfilePic
+        }
+
         // Update the User entity
         await User.createQueryBuilder()
             .update(User)
@@ -96,8 +130,9 @@ async function alterUser(user_id: number, user: UserType) {
             .where('user_id = :user_id', { user_id: user_id })
             .execute()
 
-        const updatedUser = await User.findOneBy({ user_id })
-        return updatedUser
+        const updatedUsers = await getUser(user_id)
+        return updatedUsers
+
     } catch (error) {
         // eslint-disable-next-line no-console
         console.error('Error updating user:', error)
