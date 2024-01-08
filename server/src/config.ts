5e62bb7d5bd8a5c8536a809ab5a5b1ec5f505a99
Ubuntu <gdsdt4admin@GDSDT4.1kd0uumcrpmexglh5w4lhtkfmd.fx.internal.cloudapp.net>
2024-01-08 13:34:47 +0000

    Fix prod values

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,14 +1,10 @@
 const config = {
     host: 'localhost',
     port: 3306,
-    username: 'root',
-    password: 'root',
+    username: 'gdsdt4admin',
+    password: 'gdsdt4Password$',
     database: 'ARTSYNC',
-    clientUrl: [
-        'http://localhost:5173/',
-        'http://localhost:3000',
-        'https://orange-stone-0a4f77810.4.azurestaticapps.net',
-    ],
+    clientUrl: ['http://localhost:5173/', 'http://localhost:3000', 'https://orange-stone-0a4f77810.4.azurestaticapps.net'],
 }
 
 export default config
